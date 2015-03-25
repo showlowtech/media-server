a25e0f0a362d99fd5a98993500b1633b0675a29b
yossipapi <yossi.papiashvili@kaltura.com>
2015-03-25 18:18:41 +0200

    Support php build and release

--- release.php
+++ release.php
@@ -13,8 +13,8 @@ $tag_name = "rel-$version";
 $target_commitish = exec("git rev-parse HEAD 2>&1", $output, $retVal);
 if ( $retVal !== 0 )
 {
-	echo implode("\n", $output);
-	exit( $retVal );
+    echo implode("\n", $output);
+    exit( $retVal );
 }
 
 $name = "v$version";
@@ -41,5 +41,11 @@ $filePath = __DIR__ . "/KalturaWowzaServer/build/distributions/$name";
 $contentType = 'application/zip';
 $client->repos->releases->assets->upload($owner, $repo, $releaseId, $name, $contentType, $filePath);
 echo "Installation zip file uploaded: $filePath\n";
+
+$name = "AsyncMediaServerProcessClientApp-$version.zip";
+$filePath = __DIR__ . "/KalturaWowzaServer/build/distributions/$name";
+$contentType = 'application/zip';
+$client->repos->releases->assets->upload($owner, $repo, $releaseId, $name, $contentType, $filePath);
+echo "AsyncMediaServerProcessClientApp zip file uploaded: $filePath\n";
 
 exit(0);
